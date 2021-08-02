189802b9f0c105aaa9bd200054bc5ae70ec8aac4
JimusHimus <georgii123456789@yandex.ru>
2021-08-02 15:48:22 +0300

    Prototype

--- client/main.py
+++ client/main.py
@@ -1,16 +1,31 @@
 import asyncio
 import websockets
+from websockets.exceptions import ConnectionClosedError
 
 
-async def client():
+# when client get message from server
+def handle_message(message):
+    return 'Handled message ' + message
+
+
+async def connect():
     uri = "ws://localhost:5656"
-    async with websockets.connect(uri) as websocket:
-        name = input("Type something")
-        await websocket.send(name)
+    while True:
+        async with websockets.connect(uri) as websocket:
+            await websocket.send('I\'m ready')
+            try:
+                while True:
+                    message = await websocket.recv()
+                    print(f'Got message from server: {message}')
+                    handled_message = handle_message(message)
+                    await websocket.send(handled_message)
+            except ConnectionClosedError:
+                pass
 
 
 def run_client():
-    asyncio.get_event_loop().run_until_complete(client())
+    asyncio.get_event_loop().run_until_complete(connect())
+    asyncio.get_event_loop().run_forever()
 
 
 if __name__ == '__main__':
--- server/main.py
+++ server/main.py
@@ -1,15 +1,39 @@
 import asyncio
+import aioconsole as aioconsole
 import websockets
+from websockets.exceptions import ConnectionClosedError
+
+clients = set()
 
 
-async def handle(websocket, path):
-    inp = await websocket.recv()
-    print(inp)
+# when client sent message
+def handle_message(message):
+    print(f'Got message from client: {message}')
+
+
+# when client connected
+async def handle_client(websocket, path):
+    clients.add(websocket)
+    try:
+        async for message in websocket:
+            handle_message(message)
+    except ConnectionClosedError:
+        pass
+    finally:
+        clients.remove(websocket)
+
+
+async def notify_clients():
+    while True:
+        message = await aioconsole.ainput()
+        for client in clients:
+            await client.send(message)
 
 
 def start_server():
-    server = websockets.serve(handle, "localhost", 5656)
+    server = websockets.serve(handle_client, "localhost", 5656)
     asyncio.get_event_loop().run_until_complete(server)
+    asyncio.get_event_loop().create_task(notify_clients())
     asyncio.get_event_loop().run_forever()
 
 
